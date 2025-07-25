504adad0b94025293220351e2abf8d6e2ef20236
Dionatan <zimmermanndionatan@gmail.com>
2025-07-25 12:48:01 -0500

    Added crud form on books and included controller functions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @genres = Genre.all
     @books = Book.includes(:genre, :reviews)
@@ -15,4 +17,47 @@ class BooksController < ApplicationController
     @reviews = @book.reviews.includes(:user).page(params[:page]).per(5)
     @review = Review.new
   end
+
+  def new
+    @book = Book.new
+  end
+
+   def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_url, notice: 'Book was successfully deleted.'
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :published_year, :isbn, :description, :genre_id)
+  end
 end
+
+
+
